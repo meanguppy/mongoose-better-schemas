77a2f570be53d2632332cdd946d09cdfb51537bc
Joel Nuesch <joel@meanguppy.com>
2022-11-08 17:13:42 -0800

    feat: initial

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: { '{}': false },
+        extendDefaults: true,
+      },
+    ],
+    '@typescript-eslint/lines-between-class-members': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-floating-promises': 0,
+    '@typescript-eslint/naming-convention': 0,
+    'class-methods-use-this': 0,
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'lines-between-class-members': 0,
+    'no-await-in-loop': 0,
+    'no-param-reassign': 0,
+    'no-underscore-dangle': 0,
+    'no-restricted-syntax': 0,
+    'max-classes-per-file': 0,
+    'object-curly-newline': 0,
+  },
+  ignorePatterns: ['dist', '**/*.js'],
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# mongoose-better-schemas
+
+Streamlined TypeScript schema definitions, and proper types for `select` and `populate` projections.
